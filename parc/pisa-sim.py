fa36456d395dee2497831e23f81a490c4edad8af
Derek Lockhart <lockhart@csl.cornell.edu>
2014-08-17 23:20:22 -0400

    [parc] fixes to make c translatable

--- parc/pisa-sim.py
+++ parc/pisa-sim.py
@@ -61,10 +61,10 @@ def run( state ):
 
     old = s.pc
 
-    print'{:06x}'.format( s.pc ),
+    #print'{:06x}'.format( s.pc ),
     # we use trace elidable iread instead of just read
     inst = s.mem.iread( s.pc, 4 )
-    print '{:08x}'.format( inst ), decode(inst), num_inst
+    #print '{:08x}'.format( inst ), decode(inst), num_inst
     decode( inst )( s, inst )
     num_inst += 1
 
@@ -190,11 +190,17 @@ def syscall_init( mem, argv ):
   envp = []
   argc = len( argv )
 
+  def sum_( x ):
+    val = 0
+    for i in x:
+      val += i
+    return val
+
   # calculate sizes of sections
   # TODO: parameterize auxv/envp/argv calc for variable int size?
   stack_nbytes  = [ 4,                              # end mark nbytes
-                    sum([len(x)+1 for x in envp]),  # envp_str nbytes
-                    sum([len(x)+1 for x in argv]),  # argv_str nbytes
+                    sum_([len(x)+1 for x in envp]), # envp_str nbytes
+                    sum_([len(x)+1 for x in argv]), # argv_str nbytes
                     0,                              # padding  nbytes
                     8*(len(auxv) + 1),              # auxv     nbytes
                     4*(len(envp) + 1),              # envp     nbytes
@@ -202,11 +208,11 @@ def syscall_init( mem, argv ):
                     4 ]                             # argc     nbytes
 
   # calculate padding to align boundary
-  stack_nbytes[3] = 16 - (sum(stack_nbytes[:3]) % 16)
+  stack_nbytes[3] = 16 - (sum_(stack_nbytes[:3]) % 16)
 
   # calculate stack pointer based on size of storage needed for args
   # TODO: round to nearest page size?
-  stack_ptr = stack_base - sum( stack_nbytes )
+  stack_ptr = stack_base - sum_( stack_nbytes )
   offset    = stack_base
   stack_off = []
   for nbytes in stack_nbytes:
@@ -216,26 +222,26 @@ def syscall_init( mem, argv ):
 
   # utility functions
 
-  def str_to_mem( val, addr ):
+  def str_to_mem( mem, val, addr ):
     for i, char in enumerate(val+'\0'):
       mem[ addr + i ] = char
-    return addr + i + 1
+    return addr + len(val) + 1
 
-  def int_to_mem( val, addr ):
+  def int_to_mem( mem, val, addr ):
     # TODO properly handle endianess
     for i in range( 4 ):
-      mem[addr+i] = (val >> i) & 0xFF
-    return addr + i + 1
+      mem[addr+i] = chr((val >> i) & 0xFF)
+    return addr + 4
 
   # write end marker to memory
-  int_to_mem( 0, stack_off[0] )
+  int_to_mem( mem, 0, stack_off[0] )
 
   # write environment strings to memory
   envp_ptrs = []
   offset   = stack_off[1]
   for x in envp:
     envp_ptrs.append( offset )
-    offset = str_to_mem( x, offset )
+    offset = str_to_mem( mem, x, offset )
   assert offset == stack_off[0]
 
   # write argument strings to memory
@@ -243,40 +249,40 @@ def syscall_init( mem, argv ):
   offset   = stack_off[2]
   for x in argv:
     argv_ptrs.append( offset )
-    offset = str_to_mem( x, offset )
+    offset = str_to_mem( mem, x, offset )
   assert offset == stack_off[1]
 
   # write auxv vectors to memory
   offset   = stack_off[4]
   for type_, value in auxv + [(0,0)]:
-    offset = int_to_mem( type_, offset )
-    offset = int_to_mem( value, offset )
+    offset = int_to_mem( mem, type_, offset )
+    offset = int_to_mem( mem, value, offset )
   assert offset == stack_off[3]
 
   # write envp pointers to memory
   offset   = stack_off[5]
   for env in envp_ptrs + [0]:
-    offset = int_to_mem( env, offset )
+    offset = int_to_mem( mem, env, offset )
   assert offset == stack_off[4]
 
   # write argv pointers to memory
   offset   = stack_off[6]
   for arg in argv_ptrs + [0]:
-    offset = int_to_mem( arg, offset )
+    offset = int_to_mem( mem, arg, offset )
   assert offset == stack_off[5]
 
   # write argc to memory
   offset = stack_off[7]
-  offset = int_to_mem( argc, offset )
+  offset = int_to_mem( mem, argc, offset )
   assert offset == stack_off[6]
 
   # initialize processor state
   state = State( Memory(mem), None, reset_addr=0x1000 )
 
   # initialize processor registers
-  state.rf[ reg_map['a0'] ] = argc       # argument 0 reg = argc
-  state.rf[ reg_map['a1'] ] = argv       # argument 1 reg = argv data ptr
-  state.rf[ reg_map['sp'] ] = stack_ptr  # stack pointer reg
+  state.rf[ reg_map['a0'] ] = argc         # argument 0 reg = argc
+  state.rf[ reg_map['a1'] ] = stack_off[6] # argument 1 reg = argv ptr addr
+  state.rf[ reg_map['sp'] ] = stack_ptr    # stack pointer reg
 
   return state
 
