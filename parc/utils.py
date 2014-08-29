bfe2d873886a03dd3adc8c77c0b160b09f2d6345
Derek Lockhart <lockhart@csl.cornell.edu>
2014-08-29 02:35:17 -0400

    [parc] must explicitly handle OverflowError, ugh!

--- parc/utils.py
+++ parc/utils.py
@@ -2,7 +2,6 @@
 from rpython.rlib.jit         import elidable, unroll_safe
 from rpython.rlib.rstruct     import ieee
 from rpython.rlib.rarithmetic import intmask
-#import struct
 
 #-----------------------------------------------------------------------
 # sext
@@ -37,6 +36,7 @@ def signed( value ):
 def bits2float( bits ):
   #data_str = struct.pack  ( 'I', bits     )
   #flt      = struct.unpack( 'f', data_str )[0]
+
   flt = ieee.float_unpack( bits, 4 )
   return flt
 
@@ -46,9 +46,17 @@ def bits2float( bits ):
 def float2bits( flt ):
   #data_str = struct.pack  ( 'f', flt      )
   #bits     = struct.unpack( 'I', data_str )[0]
+
   # float_pack returns an r_int rather than an int, must cast it or
   # arithmetic operations behave differently!
-  bits = trim( intmask( ieee.float_pack( flt, 4 ) ) )
+  try:
+    bits = trim( intmask( ieee.float_pack( flt, 4 ) ) )
+  # float_pack also will throw an OverflowError if the computed value
+  # won't fit in the expected number of bytes, catch this and return
+  # the encoding for inf/-inf
+  except OverflowError:
+    bits = 0x7f800000 if flt > 0 else 0xff800000
+
   return bits
 
 #-----------------------------------------------------------------------
