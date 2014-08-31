1b4a9a87983acbe95c5e554df9281bb5579f78bf
Derek Lockhart <lockhart@csl.cornell.edu>
2014-08-31 15:30:27 -0400

    [arm] add instruction field access funcs

---
+++ arm/instruction.py
@@ -0,0 +1,57 @@
+#=======================================================================
+# instruction.py
+#=======================================================================
+
+def cond( inst ):
+  return (inst >> 28) & 0xF
+
+def rn( inst ):
+  return (inst >> 16) & 0xF
+
+def rd( inst ):
+  return (inst >> 12) & 0xF
+
+def rm( inst ):
+  return inst & 0xF
+
+def rs( inst ):
+  return (inst >> 8) & 0xF
+
+def shift( inst ):
+  return (inst >> 5) & 0b11
+
+def shift_amt( inst ):
+  return (inst >> 7) & 0x1F
+
+def rotate( inst ):
+  return (inst >> 8) & 0xF
+
+def imm_8( inst ):
+  return inst & 0xFF
+
+def imm_12( inst ):
+  return inst & 0xFFF
+
+def off_24( inst ):
+  return inst & 0xFFFFFF
+
+def off_8( inst ):
+  return inst & 0xFF
+
+def cp_num( inst ):
+  return (inst >> 8) & 0xF
+
+def opcode( inst ):
+  return (inst >> 21) & 0xF
+
+def opcode_1( inst ):
+  return (inst >> 20) & 0x1F
+
+def opcode_2( inst ):
+  return (inst >> 5) & 0b111
+
+def I( inst ):
+  return (inst >> 25) & 0b1
+
+def S( inst ):
+  return (inst >> 20) & 0b1
