c20e3c79a54d79b2f58a2834ba924b91dbf3e81c
Derek Lockhart <lockhart@csl.cornell.edu>
2014-09-03 13:50:16 -0400

    [pydgin] add create_risc_decoder

--- pydgin/misc.py
+++ pydgin/misc.py
@@ -2,7 +2,10 @@
 # misc.py
 #=======================================================================
 
+import re
 import elf
+import py
+import rpython
 
 #-----------------------------------------------------------------------
 # load_program
@@ -22,3 +25,43 @@ def load_program( fp, mem ):
 
   return mem, breakpoint
 
+#-----------------------------------------------------------------------
+# create_risc_decoder
+#-----------------------------------------------------------------------
+def create_risc_decoder( encodings, isa_globals ):
+
+  inst_nbits = len( encodings[0][1] )
+
+  def split_encodings( enc ):
+    return [x for x in re.split( '(x*)', enc ) if x]
+
+  bit_fields = [ split_encodings( x[1] ) for x in encodings ]
+
+  decoder = ''
+  for i, inst in enumerate( bit_fields ):
+    #print i, encodings[i][0], inst
+    bit = 0
+    conditions = []
+    for field in reversed( inst ):
+      nbits = len( field )
+      if field[0] != 'x':
+        mask = (1 << nbits) - 1
+        cond = '(inst >> {}) & 0x{:X} == 0b{}'.format( bit, mask, field )
+        conditions.append( cond )
+      bit += nbits
+    decoder += 'if   ' if i == 0 else '  elif '
+    decoder += ' and '.join( reversed(conditions) ) + ':\n'
+    decoder += '    return execute_{}\n'.format( encodings[i][0] )
+
+  source = py.code.Source('''
+@rpython.rlib.jit.elidable
+def decode( inst ):
+  {decoder_tree}
+  else:
+    raise Exception('Invalid instruction 0x%x!' % inst )
+  '''.format( decoder_tree = decoder ))
+  #print source
+  environment = dict(globals().items() + isa_globals.items())
+  exec source.compile() in environment
+
+  return environment['decode']
