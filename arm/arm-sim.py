78fcdf8c1dc5c2f556e87becd39c9623efe6e076
Derek Lockhart <lockhart@csl.cornell.edu>
2014-09-03 13:10:51 -0400

    [arm] add arm-sim, separate out machine/bootstrap

---
+++ arm/arm-sim.py
@@ -0,0 +1,114 @@
+#=======================================================================
+# arm_sim.py
+#=======================================================================
+
+import sys
+import os
+
+#sys.path.append('/Users/dmlockhart/vc/git-brg/parc/pymtl')
+#sys.path.append('/Users/dmlockhart/vc/hg-opensource/pypy')
+sys.path.append('/work/bits0/dml257/pymtl')
+sys.path.append('/work/bits0/dml257/hg-pypy/pypy')
+
+from pydgin.misc import load_program
+
+from bootstrap   import syscall_init, memory_size
+from isa         import decode
+
+from rpython.rlib.jit import JitDriver
+
+#-----------------------------------------------------------------------
+# jit
+#-----------------------------------------------------------------------
+
+jitdriver = JitDriver( greens =['pc'],
+                       reds   =['state'],
+                       virtualizables = ['state']
+                     )
+
+def jitpolicy(driver):
+  from rpython.jit.codewriter.policy import JitPolicy
+  return JitPolicy()
+
+#-----------------------------------------------------------------------
+# run
+#-----------------------------------------------------------------------
+def run( state, debug ):
+  s = state
+
+  while s.status == 0:
+
+    jitdriver.jit_merge_point(
+      pc       = s.pc,
+      state    = s,
+    )
+
+    old = s.pc
+
+    if debug: print'{:6x}'.format( s.pc ),
+    # we use trace elidable iread instead of just read
+    inst = s.mem.iread( s.pc, 4 )
+    if debug: print '{:08x} {:8s} {:8d}'.format(inst, decode(inst).func_name[8:], s.ncycles),
+    decode( inst )( s, inst )
+    s.ncycles += 1  # TODO: should this be done inside instruction definition?
+    if s.stats_en: s.stat_ncycles += 1
+    if debug: print
+
+    #print '0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} '.format( *s.rf[ 0: 6] )
+    #print '0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} '.format( *s.rf[ 6:12] )
+    #print '0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} '.format( *s.rf[12:18] )
+    #print '0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} '.format( *s.rf[18:24] )
+    #print '0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} 0x{:08x} '.format( *s.rf[24:30] )
+    #print '0x{:08x} 0x{:08x} '.format( *s.rf[30:32] )
+
+    if s.pc < old:
+      jitdriver.can_enter_jit(
+        pc       = s.pc,
+        state    = s,
+      )
+
+  print 'DONE! Status =', s.status
+  print 'Instructions Executed =', s.ncycles
+
+#-----------------------------------------------------------------------
+# entry_point
+#-----------------------------------------------------------------------
+def entry_point( argv ):
+  try:
+    filename = argv[1]
+  except IndexError:
+    print "You must supply a filename"
+    return 1
+
+  # Load the program into a memory object
+
+  mem             = [' '] * memory_size
+  mem, breakpoint = load_program( open( filename, 'rb' ), mem )
+
+  debug = True
+
+  # Insert bootstrapping code into memory and initialize processor state
+
+  state = syscall_init( mem, breakpoint, argv, debug )
+  state.rf .debug = False
+  state.mem.debug = debug
+
+  # Execute the program
+
+  run( state, debug )
+
+  return 0
+
+#-----------------------------------------------------------------------
+# target
+#-----------------------------------------------------------------------
+# Enables RPython translation of our interpreter.
+def target( *args ):
+  return entry_point, None
+
+#-----------------------------------------------------------------------
+# main
+#-----------------------------------------------------------------------
+# Enables CPython simulation of our interpreter.
+if __name__ == "__main__":
+  entry_point( sys.argv )
---
+++ arm/machine.py
@@ -0,0 +1,83 @@
+# trace elidable for instruction reads
+from rpython.rlib.jit         import elidable, unroll_safe
+from rpython.rlib.rstruct     import ieee
+from rpython.rlib.rarithmetic import intmask
+
+#-----------------------------------------------------------------------
+# RegisterFile
+#-----------------------------------------------------------------------
+class RegisterFile( object ):
+  def __init__( self, debug=False ):
+    self.regs  = [0] * 32
+    self.debug = debug
+  def __getitem__( self, idx ):
+    if self.debug: print ':: RD.RF[%2d] = %8x' % (idx, self.regs[idx]),
+    return self.regs[idx]
+  def __setitem__( self, idx, value ):
+    if idx != 0:
+      self.regs[idx] = value
+      if self.debug: print ':: WR.RF[%2d] = %8x' % (idx, value),
+
+#-----------------------------------------------------------------------
+# Memory
+#-----------------------------------------------------------------------
+class Memory( object ):
+  def __init__( self, data=None, debug=False ):
+    if not data:
+      self.data = [' ']*2**10
+    else:
+      self.data = data
+    self.debug = debug
+
+  @unroll_safe
+  def read( self, start_addr, num_bytes ):
+    value = 0
+    if self.debug: print ':: RD.MEM[%x] = ' % (start_addr),
+    for i in range( num_bytes-1, -1, -1 ):
+      value = value << 8
+      value = value | ord( self.data[ start_addr + i ] )
+    if self.debug: print '%x' % (value),
+    return value
+
+  # this is instruction read, which is otherwise identical to read. The
+  # only difference is the elidable annotation, which we assume the
+  # instructions are not modified (no side effects, assumes the addresses
+  # correspond to the same instructions)
+  @elidable
+  @unroll_safe
+  def iread( self, start_addr, num_bytes ):
+    value = 0
+    for i in range( num_bytes-1, -1, -1 ):
+      value = value << 8
+      value = value | ord( self.data[ start_addr + i ] )
+    return value
+
+  @unroll_safe
+  def write( self, start_addr, num_bytes, value ):
+    if self.debug: print ':: WR.MEM[%x] = %x' % (start_addr, value),
+    for i in range( num_bytes ):
+      self.data[ start_addr + i ] = chr(value & 0xFF)
+      value = value >> 8
+
+#-----------------------------------------------------------------------
+# State
+#-----------------------------------------------------------------------
+class State( object ):
+  _virtualizable_ = [ 'rf.regs[*]' ]
+  def __init__( self, memory, symtable, reset_addr=0x400, debug=False ):
+    self.pc       = reset_addr
+    self.rf       = RegisterFile()
+    self.mem      = memory
+
+    self.rf .debug = debug
+    self.mem.debug = debug
+
+    # coprocessor registers
+    self.status        = 0
+    self.stats_en      = 0
+    self.ncycles       = 0
+    self.stat_ncycles  = 0
+
+    # syscall stuff... TODO: should this be here?
+    self.breakpoint = 0
+
