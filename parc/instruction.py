70f8752cba94a800f1455269529b52abf3b759a7
bi45 <bi45@cornell.edu>
2015-04-03 11:46:22 -0400

    [parc] add @property decorator to instruction fields

--- parc/instruction.py
+++ parc/instruction.py
@@ -7,30 +7,39 @@ class Instruction( object ):
     self.bits = bits
     self.str  = str
 
+  @property
   def rd( self ):
     return (self.bits >> 11) & 0x1F
 
+  @property
   def rt( self ):
     return (self.bits >> 16) & 0x1F
 
+  @property
   def rs( self ):
     return (self.bits >> 21) & 0x1F
 
+  @property
   def fd( self ):
     return (self.bits >>  6) & 0x1F
 
+  @property
   def ft( self ):
     return (self.bits >> 16) & 0x1F
 
+  @property
   def fs( self ):
     return (self.bits >> 11) & 0x1F
 
+  @property
   def imm( self ):
     return self.bits & 0xFFFF
 
+  @property
   def jtarg( self ):
     return self.bits & 0x3FFFFFF
 
+  @property
   def shamt( self ):
     return (self.bits >> 6) & 0x1F
 
