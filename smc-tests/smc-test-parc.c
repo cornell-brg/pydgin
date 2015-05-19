9fd231df327b71286d4746b9b919013ce479ba74
bi45 <bi45@cornell.edu>
2015-05-19 15:57:46 -0400

    [smc-tests] made the smc test a bit more complicated

--- smc-tests/smc-test-parc.c
+++ smc-tests/smc-test-parc.c
@@ -18,6 +18,11 @@ int loop_parc( int n ) {
         unsigned int *incr_label_ptr;
         incr_label_ptr = (unsigned int *) &&incr_label;
         *incr_label_ptr = 0x2610ffff;
+      } else if ( i == 160 && j == 50 ) {
+        // modify incr_label with addiu sum_r, sum_r, 2
+        unsigned int *incr_label_ptr;
+        incr_label_ptr = (unsigned int *) &&incr_label;
+        *incr_label_ptr = 0x26100002;
       }
 
     }
@@ -41,15 +46,15 @@ int main() {
 
   int sum;
   #ifdef _MIPS_ARCH_MAVEN
-  sum = loop_parc( 100 );
+  sum = loop_parc( 250 );
   #else
-  sum = loop( 100 );
+  sum = loop( 250 );
   #endif
   printf( "sum: %d\n", sum );
-  if ( sum == 8102 ) {
+  if ( sum == 49949 ) {
     printf( "TEST PASSED!\n" );
   } else {
-    printf( "TEST FAILED! Was expecting 8102\n" );
+    printf( "TEST FAILED! Was expecting 49949\n" );
   }
   return 0;
 
