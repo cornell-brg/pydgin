1819b7bf7420923162f2f1a0080bda35510af91d
Berkin Ilbeyi <bi45@cornell.edu>
2015-06-10 13:21:32 -0700

    [scripts] move tutorial bins to parc dir

--- scripts/prepare-tut.sh
+++ scripts/prepare-tut.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # remove, mkdir and configure build dirs for tutorial preparation
+echo "move the pydgin binaries"
+cp tutorial-bins/* ../parc/
+
 echo "cleaning parc/asm_tests"
 cd ../parc/asm_tests;
 rm -rf build/;
