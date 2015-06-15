e05f8c5b367eee972af70df2d382a8aafa201131
Berkin Ilbeyi <berkin@csl.cornell.edu>
2015-06-15 15:33:50 -0700

    [riscv] remove parc junk

--- riscv/bootstrap.py
+++ riscv/bootstrap.py
@@ -10,13 +10,6 @@ from machine import State
 # initialize simulator state for simple programs, no syscalls
 def test_init( mem, debug ):
 
-  # inject bootstrap code into the memory
-
-  for i, data in enumerate( bootstrap_code ):
-    mem.write( bootstrap_addr + i, 1, data )
-  for i, data in enumerate( rewrite_code ):
-    mem.write( rewrite_addr + i, 1, data )
-
   # instantiate architectural state with memory and reset address
 
   state = State( mem, debug, reset_addr=0x2000 )
