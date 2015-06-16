ec5ca232f69df40337028e8d024ccc542e05be22
Berkin Ilbeyi <berkin@csl.cornell.edu>
2015-06-16 10:39:08 -0700

    [riscv] zero upper bits if not signed in sext_32

--- riscv/utils.py
+++ riscv/utils.py
@@ -11,6 +11,7 @@ def trim_64( value ):
   return value & 0xffffffffffffffff
 
 def sext_32( value ):
+  value = 0x00000000ffffffff & value
   if value & 0x80000000:
     return 0xffffffff00000000 | value
   return value
