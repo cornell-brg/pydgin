60fe94188dd916a11544c8f213efad2009a8c719
Berkin Ilbeyi <berkin@csl.cornell.edu>
2015-06-16 15:03:49 -0700

    [riscv] print test summary

--- riscv/test-all.py
+++ riscv/test-all.py
@@ -14,6 +14,11 @@ print "looking for tests in", in_dir
 
 dumps = glob.glob( in_dir + "/*.dump" )
 
+num_tests  = 0
+num_passed = 0
+num_failed = 0
+num_error  = 0
+
 for dump in dumps:
   bin_name = dump[:-5]
   try:
@@ -22,10 +27,19 @@ for dump in dumps:
   except subprocess.CalledProcessError as e:
     out = e.output
 
-  out_str = "Error"
+  num_tests += 1
+
   if "Pass!" in out:
     out_str = "Passed"
+    num_passed += 1
   elif "Fail!" in out:
     out_str = "Failed"
+    num_failed += 1
+  else:
+    out_str = "Error"
+    num_error += 1
   test_name = bin_name[ bin_name.rfind('/')+1 : ]
   print "Test {} \t {}".format( test_name, out_str )
+
+print "Number of tests: {} passed: {} failed: {} error: {}" \
+    .format( num_tests, num_passed, num_failed, num_error  )
