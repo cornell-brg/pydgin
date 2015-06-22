6769a92f8898bbfbc554852136a2eddf95db8d76
Derek Lockhart <lockhart@csl.cornell.edu>
2015-06-22 13:19:04 -0400

    [riscv] horrible hack for register file issue

--- riscv/machine.py
+++ riscv/machine.py
@@ -70,14 +70,73 @@ class RiscVRegisterFile( RegisterFile ):
   def __setitem__( self, idx, value ):
     return RegisterFile.__setitem__( self, idx, trim_64( value ) )
 
-class RiscVFPRegisterFile( RegisterFile ):
-  def __init__( self ):
-    RegisterFile.__init__( self,
-      constant_zero=False,
-      num_regs=32,
-      nbits=64
-    )
+#class RiscVFPRegisterFile( RegisterFile ):
+#  def __init__( self ):
+#    RegisterFile.__init__( self,
+#      constant_zero=False,
+#      num_regs=32,
+#      nbits=64
+#    )
+#
+#  @specialize.argtype(2)
+#  def __setitem__( self, idx, value ):
+#    return RegisterFile.__setitem__( self, idx, value )
+
+#-------------------------------------------------------------------------
+# Hacky RPython Workaround
+#-------------------------------------------------------------------------
+from pydgin.utils import r_uint
+from pydgin.debug import Debug, pad, pad_hex
+
+class RiscVFPRegisterFile( object ):
+  def __init__( self, constant_zero=False, num_regs=32, nbits=64 ):
+    self.num_regs = num_regs
+    self.regs     = [ r_uint(0) ] * self.num_regs
+    self.debug    = Debug()
+    self.nbits    = nbits
+    self.debug_nchars = nbits / 4
+
+    if constant_zero: self._setitemimpl = self._set_item_const_zero
+    else:             self._setitemimpl = self._set_item
+  def __getitem__( self, idx ):
+    if self.debug.enabled( "rf" ):
+      print ':: RD.RF[%s] = %s' % (
+                          pad( "%d" % idx, 2 ),
+                          pad_hex( self.regs[idx],
+                                   len=self.debug_nchars ) ),
+    return self.regs[idx]
 
   @specialize.argtype(2)
   def __setitem__( self, idx, value ):
-    return RegisterFile.__setitem__( self, idx, value )
+    value = r_uint( value )
+    self._setitemimpl( idx, value )
+
+  def _set_item( self, idx, value ):
+    self.regs[idx] = value
+    if self.debug.enabled( "rf" ):
+      print ':: WR.RF[%s] = %s' % (
+                        pad( "%d" % idx, 2 ),
+                        pad_hex( self.regs[idx],
+                                 len=self.debug_nchars ) ),
+  def _set_item_const_zero( self, idx, value ):
+    if idx != 0:
+      self.regs[idx] = value
+      if self.debug.enabled( "rf" ):
+        print ':: WR.RF[%s] = %s' % (
+                          pad( "%d" % idx, 2 ),
+                          pad_hex( self.regs[idx],
+                                   len=self.debug_nchars ) ),
+
+  #-----------------------------------------------------------------------
+  # print_regs
+  #-----------------------------------------------------------------------
+  # prints all registers (register dump)
+  # per_row specifies the number of registers to display per row
+  def print_regs( self, per_row=6 ):
+    for c in xrange( 0, self.num_regs, per_row ):
+      str = ""
+      for r in xrange( c, min( self.num_regs, c+per_row ) ):
+        str += "%s:%s " % ( pad( "%d" % r, 2 ),
+                            pad_hex( self.regs[r] ) )
+      print str
+
