441aac435a34971dba603aa83cb78bd3cfff73df
ka429 <ka429@cornell.edu>
2016-03-28 13:49:36 -0400

    [parc][amo] Reverting changes to pydgin.utils and use trim_32 for unsigned
    https://github.com/cornell-brg/pydgin/pull/32#issuecomment-202474822

--- parc/isa.py
+++ parc/isa.py
@@ -3,8 +3,8 @@
 #=======================================================================
 
 from        utils import trim_5
-from pydgin.utils import signed , unsigned  , sext_16   , sext_8, \
-                         trim_32, bits2float, float2bits, r_uint
+from pydgin.utils import signed    , sext_16   , sext_8, trim_32, \
+                         bits2float, float2bits, r_uint
 
 from pydgin.misc import create_risc_decoder, FatalError
 
@@ -783,7 +783,7 @@ def execute_amo_xchg( s, inst ):
 #-----------------------------------------------------------------------
 def execute_amo_min( s, inst ):
   temp = s.mem.read( s.rf[ inst.rs ], 4 )
-  s.mem.write( s.rf[inst.rs], 4, unsigned( min( signed( temp ), signed( s.rf[inst.rt] ) ) ) )
+  s.mem.write( s.rf[inst.rs], 4, trim_32( min( signed( temp ), signed( s.rf[inst.rt] ) ) ) )
   s.rf[ inst.rd ] = temp
   s.pc += 4
 
