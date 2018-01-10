669b064d1fe24a07a64e61c566c66bb481918ad6
Shreesha Srinath <shreesha@csl.cornell.edu>
2018-01-10 14:20:43 -0500

    Fixes to index transform experiments

--- trace-utils/tools/index-transforms.py
+++ trace-utils/tools/index-transforms.py
@@ -14,42 +14,59 @@ if __name__ == "__main__":
   kp    = k*p  # logical cores
   cl_sz = 4    # cache line size
 
-  if n%kp == 0:
-    tk_sz = n/kp
-  else:
-    tk_sz = int(math.ceil(n/kp+1)) # task size
+  # Select the task-size constraint
+  tk_sz = int(math.ceil(n/float(kp))) # task size
 
-  # cache-layout
+  # Cache layout
   print "Cache Layout"
-  num_lines = n/cl_sz if n%cl_sz == 0 else int(math.ceil(n/cl_sz)+1)
+  num_lines = int(math.ceil(n/float(cl_sz)))
   for i in range(num_lines):
     end = (i+1)*cl_sz if (i+1)*cl_sz < n else n
     print ' '.join(['%4d' % i for i in range(i*cl_sz, end)])
 
   print "Transform to"
-  # Check the relationship of
-  #  kp, tk_sz, cl_sz
-  if kp <= cl_sz and tk_sz >= kp:
-    cl_sz = kp
 
+  # Check the number of available processors and cache-line size
+  # NOTE: Assuming that "p" processors execute in space
+  if p <= cl_sz:
+    cl_sz = p
+
+  # data should be accessed as below:
+  #
+  #                  <---task-size--->
+  #
+  #      ^        ^     beg      end
+  #      |        |  p0
+  #      |        |  p1
+  #      | cl_sz  |  p2   Group 0
+  #      |        |  p3
+  #      |        |  p4
+  #      |        v
+  #      |          <---task-size--->
+  # kp   |
+  #      |       ^     beg      end
+  #      |       |  p0
+  #      |       |  p1
+  #      |cl_sz  |  p2   Group 1
+  #      |       |  p3
+  #      |       |  p4
+  #      v       v
+
+  # for each task print the suggested access pattern
   for i in range(kp):
-    beg = i%cl_sz + (i/cl_sz)*(tk_sz*cl_sz)
-    end = beg + (tk_sz-1)*cl_sz + 1
-    stride = cl_sz
+
+    group_sz  = (tk_sz * cl_sz)
+    group_beg = (i/cl_sz)*group_sz
+
+    if (n - group_beg) < group_sz:
+      stride = int( math.ceil((n-group_beg)/float(tk_sz)) )
+    else:
+      stride = cl_sz
+
+    beg = i%cl_sz+ (i/cl_sz)*(tk_sz*cl_sz)
+    end = beg + tk_sz*stride
 
     if end > n:
       end = n
 
-    # NOTE: THIS NEEDS TO BE FIXED
-    # eg test: cl_sz=6, kp=8, n=32
-    # unequal divisions...
-    #if end > n and beg+end > n:
-    #  beg = i*tk_sz
-    #  end = (i+1)*tk_sz if (i+1)*tk_sz < n else n
-    #  stride = 1
-    ## cap the end
-    #elif end > n:
-    #  end = n
-
     print ' '.join(['%4d' % i for i in range(beg, end, stride)])
-
