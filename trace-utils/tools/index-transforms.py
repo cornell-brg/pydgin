9b7530bae46101afd334e434646906ff546faaf0
Shreesha Srinath <shreesha@csl.cornell.edu>
2018-01-09 22:49:11 -0500

    some fixes
    revisit this script...needs way more testing!

--- trace-utils/tools/index-transforms.py
+++ trace-utils/tools/index-transforms.py
@@ -8,35 +8,48 @@ import math
 
 if __name__ == "__main__":
 
-  n     = 100   # problem size
-  kp    = 8     # logical cores
-  cl_sz = 16    # cache line size
-  tk_sz = n/kp  # task size
+  n     = 100  # problem size
+  k     = 2    # decomposition factor
+  p     = 4    # number of physical cores
+  kp    = k*p  # logical cores
+  cl_sz = 4    # cache line size
+
+  if n%kp == 0:
+    tk_sz = n/kp
+  else:
+    tk_sz = int(math.ceil(n/kp+1)) # task size
 
   # cache-layout
   print "Cache Layout"
-  for i in range(int(math.ceil( n/cl_sz ))):
-    end = (i+1)*cl_sz if i+cl_sz < n else n
+  num_lines = n/cl_sz if n%cl_sz == 0 else int(math.ceil(n/cl_sz)+1)
+  for i in range(num_lines):
+    end = (i+1)*cl_sz if (i+1)*cl_sz < n else n
     print ' '.join(['%4d' % i for i in range(i*cl_sz, end)])
-  print
 
   print "Transform to"
   # Check the relationship of
   #  kp, tk_sz, cl_sz
   if kp <= cl_sz and tk_sz >= kp:
     cl_sz = kp
+
   for i in range(kp):
     beg = i%cl_sz + (i/cl_sz)*(tk_sz*cl_sz)
-    end = beg + (tk_sz-1)*cl_sz
+    end = beg + (tk_sz-1)*cl_sz + 1
     stride = cl_sz
-    # cap the end
-    if end > n and beg+end < n:
-      end = n
-    # unequal divisions...
-    elif end > n and beg+end > n:
-      beg = i*tk_sz
-      end = (i+1)*tk_sz-1 if (i+1)*tk_sz-1 < n else n
-      stride = 1
 
-    print ' '.join(['%4d' % i for i in range(beg, end+1, stride)])
+    if end > n:
+      end = n
+
+    # NOTE: THIS NEEDS TO BE FIXED
+    # eg test: cl_sz=6, kp=8, n=32
+    # unequal divisions...
+    #if end > n and beg+end > n:
+    #  beg = i*tk_sz
+    #  end = (i+1)*tk_sz if (i+1)*tk_sz < n else n
+    #  stride = 1
+    ## cap the end
+    #elif end > n:
+    #  end = n
+
+    print ' '.join(['%4d' % i for i in range(beg, end, stride)])
 
