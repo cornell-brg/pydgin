a0d1b014c1adc80902707dfb4ab2be48724436ad
Shreesha Srinath <shreesha@csl.cornell.edu>
2018-01-19 00:54:50 -0500

    Saving something useful...

--- trace-utils/scripts/workflow_debug.py
+++ trace-utils/scripts/workflow_debug.py
@@ -8,46 +8,36 @@ from doit_pydgin_utils import *
 
 def task_pydgin_sims_debug():
 
-  ncores       = 4
-  l0_buffer_sz = 1
-  ports        = 4
-  llfus        = 4
-  lockstep     = 1
-  analysis     = 1
-
-  # get an evaluation dictionary
   evaldict = get_base_evaldict()
 
   # task info
-  evaldict['basename']    = "sim-pydgin-debug"
-  evaldict['resultsdir']  = "results-debug"
-  evaldict['doc']         = os.path.basename(__file__).rstrip('c')
+  evaldict['basename']        = "sim-debug"
+  evaldict['resultsdir']      = "results-debug"
+  evaldict['doc']             = os.path.basename(__file__).rstrip('c')
 
-  # kernels to run with options
-  evaldict['app_group']   = ["tiny","mtpull"]
-  #evaldict['app_list']    = ['cilk-matmul-parc-mtpull']
-  #evaldict['app_list']    = ['pbbs-bfs-ndBFS-parc-mtpull']
-  evaldict['app_list']    = ['ubmark-vvadd']
-  evaldict['app_dict']    = app_dict
+  # bmark params
+  evaldict['app_group']       = ["tiny","mtpull"]
+  evaldict['app_list']        = ['cilk-heat-parc-mtpull']
+  evaldict['app_dict']        = app_dict
 
-  # pydgin options
-  evaldict['ncores']         = ncores           # number of cores to simulate
-  evaldict['inst_ports']     = ports            # instruction port bw
-  evaldict['l0_buffer_sz']   = l0_buffer_sz     # l0 buffer size
-  evaldict['icache_line_sz'] = 16               # icache line size
-  evaldict['data_ports']     = ports            # data port bw
-  evaldict['mdu_ports']      = llfus            # mdu port bw
-  evaldict['fpu_ports']      = llfus            # fpu port bw
-  evaldict['analysis']       = analysis         # type of reconvergence scheme
-  evaldict['lockstep']       = bool( lockstep ) # enable lockstep execution
-  evaldict['cluster']        = True             # enable running the job on cluster
-  evaldict['runtime']        = True             # provide runtime metadata
-  evaldict['extra_app_opts'] = "--verify"
+  # uarch params
+  evaldict['barrier_limit']   = 250
+  evaldict['ncores']          = 4
+  evaldict['l0_buffer_sz']    = 0
+  evaldict['icache_line_sz']  = 16
+  evaldict['dcache_line_sz']  = 16
+  evaldict['inst_ports']      = 4
+  evaldict['data_ports']      = 4
+  evaldict['mdu_ports']       = 4
+  evaldict['fpu_ports']       = 4
+  evaldict['analysis']        = 1
+
+  # misc params
+  evaldict['cluster']         = False
+  evaldict['runtime']         = True
 
   # debug options
-  evaldict['linetrace']   = True
-  evaldict['color']       = True
+  evaldict['linetrace']       = True
+  evaldict['color']           = True
 
   yield gen_trace_per_app( evaldict )
-
-
