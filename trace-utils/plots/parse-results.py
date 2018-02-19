97c948da4e5cec91860ecd7fa3afd373bf32a96a
Shreesha Srinath <shreesha@csl.cornell.edu>
2018-02-19 09:06:42 -0500

    Edits to plots

--- trace-utils/plots/parse-results.py
+++ trace-utils/plots/parse-results.py
@@ -28,7 +28,7 @@ def execute(cmd):
 # global variables
 #-------------------------------------------------------------------------
 
-g_prefix_path = "../four-core/"
+g_prefix_path = "../fast-spin/"
 
 g_configs_dict = {}
 
@@ -72,7 +72,13 @@ def summarize():
 
   with open( res_file, 'w' ) as out:
 
-    out.write('app,config,steps,isavings,l0_hits,icoalesces,frontend,execute,dsavings,total_savings\n')
+    columns  = 'app,config,steps,total_insts,'
+    columns += 'unique_iaccess,total_iaccess,total_l0_hits,total_coalesces,'
+    columns += 'unique_frontend,total_frontend,'
+    columns += 'unique_execute,total_execute,'
+    columns += 'unique_daccess,total_daccess,'
+    columns += 'unique_work,total_work\n'
+    out.write( columns )
 
     for config_dir,config in g_configs_dict.iteritems():
       resultsdir_path = g_prefix_path + config_dir
@@ -90,25 +96,28 @@ def summarize():
             continue
 
           res_file =  resultsdir_path + '/' + subfolder + '/' + subfolder + '.out'
-          cmd      = 'grep -r -A 70 "Serial steps in stats region =" %(res_file)s' % { 'res_file' : res_file }
+          cmd      = 'grep -r -A 90 "Serial steps in stats region =" %(res_file)s' % { 'res_file' : res_file }
           lines    = execute( cmd )
 
-          steps          = 0
-          unique_iaccess = 0
-          unique_front   = 0
-          unique_execute = 0
-          unique_daccess = 0
-          total_iaccess  = 0
-          total_front    = 0
-          total_execute  = 0
-          total_daccess  = 0
-          total_l0_hits  = 0
-          total_coalesce = 0
+          steps           = 0
+          unique_iaccess  = 0
+          unique_frontend = 0
+          unique_execute  = 0
+          unique_daccess  = 0
+          total_iaccess   = 0
+          total_frontend  = 0
+          total_execute   = 0
+          total_daccess   = 0
+          total_l0_hits   = 0
+          total_coalesces = 0
+          total_insts     = 0
 
           for line in lines.split('\n'):
             if line != '':
               if   'Total steps in stats region' in line:
                 steps = int( line.split()[-1] )
+              elif 'Total insts in parallel regions' in line:
+                total_insts = int( line.split()[-1] )
               elif 'Total instruction accesses in parallel regions' in line:
                 total_iaccess = int( line.split()[-1] )
               elif 'Unique instruction accesses in parallel regions' in line:
@@ -116,11 +125,11 @@ def summarize():
               elif 'Total hits in Core L0 buffer' in line:
                 total_l0_hits = int( line.split()[-1] )
               elif 'Total number of coalesced instruction accesses' in line:
-                total_coalesce = int( line.split()[-1] )
+                total_coalesces = int( line.split()[-1] )
               elif 'Total frontend accesses in parallel regions' in line:
-                total_front = int( line.split()[-1] )
+                total_frontend = int( line.split()[-1] )
               elif 'Unique frontend accesses in parallel regions' in line:
-                unique_front = int( line.split()[-1] )
+                unique_frontend = int( line.split()[-1] )
               elif 'Total number of executed instructions' in line:
                 total_execute = int( line.split()[-1] )
               elif 'Unique executed instructions' in line:
@@ -130,26 +139,46 @@ def summarize():
               elif 'Unique data accesses in parallel regions' in line:
                 unique_daccess = int( line.split()[-1] )
 
+          total_work    = total_iaccess + total_frontend + total_execute + total_daccess
+          unique_work   = unique_iaccess + unique_frontend + unique_execute + unique_daccess
 
-          total_work    = total_iaccess + total_front + total_execute + total_daccess
-          unique_work   = unique_iaccess + unique_front + unique_execute + unique_daccess
-          total_savings = 100*float( total_work - unique_work )/total_work
-          l0_hits       = 100*float( total_l0_hits )/total_work
-          icoalesces    = 100*float( total_coalesce )/total_work
-          frontend_redn = 100*float( total_front - unique_front )/total_work
-          value_redn    = 100*float( total_execute - unique_execute )/total_work
-          dsavings      = 100*float( total_daccess - unique_daccess )/total_work
-          isavings      = 100*float( total_iaccess - unique_iaccess )/total_work
+          #total_savings = 100*float( total_work - unique_work )/total_work
+          #l0_hits       = 100*float( total_l0_hits )/total_work
+          #icoalesces    = 100*float( total_coalesce )/total_work
+          #frontend_redn = 100*float( total_front - unique_front )/total_work
+          #value_redn    = 100*float( total_execute - unique_execute )/total_work
+          #dsavings      = 100*float( total_daccess - unique_daccess )/total_work
+          #isavings      = 100*float( total_iaccess - unique_iaccess )/total_work
+
+          base_str = '{},'*15 + '{}\n'
           if config == "serial":
-            out.write('{},{},{},{},{},{},{},{},{},{}\n'.format(app,config,steps,isavings,l0_hits,icoalesces,frontend_redn,value_redn,dsavings,total_savings))
+            out.write(
+              base_str.format(
+                app,config,steps,total_insts,
+                unique_iaccess,total_iaccess,total_l0_hits,total_coalesces,
+                unique_frontend,total_frontend,
+                unique_execute,total_execute,
+                unique_daccess,total_daccess,
+                unique_work,total_work
+              )
+            )
           else:
-            out.write('{},{},{},{},{},{},{},{},{},{}\n'.format(app_short_name_dict[app],config,steps,isavings,l0_hits,icoalesces,frontend_redn,value_redn,dsavings,total_savings))
+            out.write(
+              base_str.format(
+                app_short_name_dict[app],config,steps,total_insts,
+                unique_iaccess,total_iaccess,total_l0_hits,total_coalesces,
+                unique_frontend,total_frontend,
+                unique_execute,total_execute,
+                unique_daccess,total_daccess,
+                unique_work,total_work
+              )
+            )
         except:
           print "{} {}: Results file not present".format( config, subfolder )
           if config == "serial":
-            out.write('{},{},{},{},{},{},{},{},{},{}\n'.format(app,config,0,0,0,0,0,0,0,0))
+            out.write( base_str.format(app,config,0,0,0,0,0,0,0,0,0,0,0,0,0,0) )
           else:
-            out.write('{},{},{},{},{},{},{},{},{},{}\n'.format(app_short_name_dict[app],config,0,0,0,0,0,0,0,0))
+            out.write( base_str.format(app_short_name_dict[app],config,0,0,0,0,0,0,0,0,0,0,0,0,0,0) )
           continue
 
 #-------------------------------------------------------------------------
