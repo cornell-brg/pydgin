0a2b4886e882d2a20760c8446ce9bbce6d98f6ac
Shreesha Srinath <shreesha@csl.cornell.edu>
2018-02-19 13:44:40 -0500

    Adding a script to run the plots for the entire design space

--- trace-utils/plots/pdf-merge.py
+++ trace-utils/plots/pdf-merge.py
@@ -17,7 +17,7 @@ from common import *
 def execute(cmd):
   try:
     print cmd
-    #return subprocess.check_output(cmd, shell=True)
+    return subprocess.check_output(cmd, shell=True)
   except  subprocess.CalledProcessError, err:
     return err
 
@@ -31,13 +31,12 @@ g_path = './'
 # merge_pdfs
 #-------------------------------------------------------------------------
 
-def merge_pdfs( outfile ):
+def merge_pdfs( config, outfile ):
   merge_files = []
   for app in app_list:
-    if not os.path.isfile( g_path + app + '-scatter.pdf' ):
+    if not os.path.isfile( g_path + app + '-%.pdf' % config ):
       continue
-    merge_files.append( g_path + app + '-scatter.pdf' )
-    merge_files.append( g_path + app + '-bar.pdf' )
+    merge_files.append( g_path + app + '-%.pdf' % config )
   merge_files = ' '.join( merge_files )
   cmd = 'pdftk ' + merge_files + ' output ' + outfile
   execute( cmd )
@@ -47,7 +46,7 @@ def merge_pdfs( outfile ):
 #-------------------------------------------------------------------------
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print "Enter the output file!"
+  if len(sys.argv) != 3:
+    print "Enter the configuration and the output file!"
     exit( 1 )
-  merge_pdfs( sys.argv[1] )
+  merge_pdfs( sys.argv[1], sys.argv[2] )
---
+++ trace-utils/plots/run-design-space.py
@@ -0,0 +1,127 @@
+#=========================================================================
+# run-design-space.py
+#=========================================================================
+# Script runs the design space
+
+import os
+import re
+import sys
+import subprocess
+
+#-------------------------------------------------------------------------
+# Utility Function
+#-------------------------------------------------------------------------
+
+def execute(cmd):
+  try:
+    print cmd
+    return subprocess.check_output(cmd, shell=True)
+  except  subprocess.CalledProcessError, err:
+    return err
+
+#-------------------------------------------------------------------------
+# main
+#-------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+  # clean up the existing results
+  if os.path.exists( "results" ):
+    cmd = 'rm -r results'
+    execute( cmd )
+
+  # 1. mimd results
+  for rt in ['spmd','wsrt']:
+    cmd = 'mkdir -p results/mimd/%s' % rt
+    execute( cmd )
+    cmds = []
+    cmds.append( './tpa-mimd-dsa --%s' % rt )
+    cmds.append( 'cp *.pdf results/mimd/%s/.' % rt )
+    cmds.append( 'cp common.py results/mimd/%s/.' % rt )
+    cmds.append( 'cp pdf-merge.py results/mimd/%s/.' % rt )
+    cmds.append( 'rm *.pdf' )
+    with open( 'results/mimd/%s/%s.log' % ( rt, rt ), 'w' ) as out:
+      for cmd in cmds:
+        log = execute( cmd )
+        out.write( log )
+
+  # 2. mimd-static results
+  for rt in ['spmd','wsrt']:
+    for insn_ports in [1,2]:
+      cmd = 'mkdir -p results/mimd-static-%dI/%s' % ( insn_ports, rt )
+      execute( cmd )
+      cmds = []
+      cmds.append( './tpa-mimd-static-dsa --g_ncores 4 --g_insn_ports %d --g_resources 4 --%s' % ( insn_ports, rt ) )
+      cmds.append( 'cp *.pdf results/mimd-static-%dI/%s/.' % ( insn_ports, rt ) )
+      cmds.append( 'cp common.py results/mimd-static-%dI/%s/.' % ( insn_ports, rt ) )
+      cmds.append( 'cp pdf-merge.py results/mimd-static-%dI/%s/.' % ( insn_ports, rt ) )
+      cmds.append( 'rm *.pdf' )
+      with open( 'results/mimd-static-%dI/%s/%s.log' % ( insn_ports, rt, rt ), 'w' ) as out:
+        for cmd in cmds:
+          log = execute( cmd )
+          out.write( log )
+
+  # 3. ccores results
+  for rt in ['spmd','wsrt']:
+    for insn_ports in [1,2]:
+      for resources in [1,2]:
+        cmd = 'mkdir -p results/ccores-%dI-%dL/%s' % ( insn_ports, resources, rt )
+        execute( cmd )
+        cmds = []
+        cmds.append( './tpa-ccores-dsa --g_ncores 4 --g_insn_ports %d --g_resources %d --%s' % ( insn_ports, resources, rt ) )
+        cmds.append( 'cp *.pdf results/ccores-%dI-%dL/%s/.' % ( insn_ports, resources, rt ) )
+        cmds.append( 'cp common.py results/ccores-%dI-%dL/%s/.' % ( insn_ports, resources, rt ) )
+        cmds.append( 'cp pdf-merge.py results/ccores-%dI-%dL/%s/.' % ( insn_ports, resources, rt ) )
+        cmds.append( 'rm *.pdf' )
+        with open( 'results/ccores-%dI-%dL/%s/%s.log' % ( insn_ports, resources, rt, rt ), 'w' ) as out:
+          for cmd in cmds:
+            log = execute( cmd )
+            out.write( log )
+
+  # 4. simt-static results
+  for rt in ['spmd','wsrt']:
+    for insn_ports in [1,2]:
+      cmd = 'mkdir -p results/simt-static-%dI/%s' % ( insn_ports, rt )
+      execute( cmd )
+      cmds = []
+      cmds.append( './tpa-simt-static-dsa --g_ncores 4 --g_insn_ports %d --g_resources 4 --%s' % ( insn_ports, rt ) )
+      cmds.append( 'cp *.pdf results/simt-static-%dI/%s/.' % ( insn_ports, rt ) )
+      cmds.append( 'cp common.py results/simt-static-%dI/%s/.' % ( insn_ports, rt ) )
+      cmds.append( 'cp pdf-merge.py results/simt-static-%dI/%s/.' % ( insn_ports, rt ) )
+      cmds.append( 'rm *.pdf' )
+      with open( 'results/simt-static-%dI/%s/%s.log' % ( insn_ports, rt, rt ), 'w' ) as out:
+        for cmd in cmds:
+          log = execute( cmd )
+          out.write( log )
+
+  # 5. simt results
+  for rt in ['spmd','wsrt']:
+    for insn_ports in [1,2]:
+      for resources in [1,2]:
+        cmd = 'mkdir -p results/simt-%dI-%dL/%s' % ( insn_ports, resources, rt )
+        execute( cmd )
+        cmds = []
+        cmds.append( './tpa-simt-dsa --g_ncores 4 --g_insn_ports %d --g_resources %d --%s' % ( insn_ports, resources, rt ) )
+        cmds.append( 'cp *.pdf results/simt-%dI-%dL/%s/.' % ( insn_ports, resources, rt ) )
+        cmds.append( 'cp common.py results/simt-%dI-%dL/%s/.' % ( insn_ports, resources, rt ) )
+        cmds.append( 'cp pdf-merge.py results/simt-%dI-%dL/%s/.' % ( insn_ports, resources, rt ) )
+        cmds.append( 'rm *.pdf' )
+        with open( 'results/simt-%dI-%dL/%s/%s.log' % ( insn_ports, resources, rt, rt ), 'w' ) as out:
+          for cmd in cmds:
+            log = execute( cmd )
+            out.write( log )
+
+  # 6. mt results
+  for rt in ['spmd','wsrt']:
+    cmd = 'mkdir -p results/mt/%s' % rt
+    execute( cmd )
+    cmds = []
+    cmds.append( './tpa-mt-dsa --%s' % rt )
+    cmds.append( 'cp *.pdf results/mt/%s/.' % rt )
+    cmds.append( 'cp common.py results/mt/%s/.' % rt )
+    cmds.append( 'cp pdf-merge.py results/mt/%s/.' % rt )
+    cmds.append( 'rm *.pdf' )
+    with open( 'results/mt/%s/%s.log' % ( rt, rt ), 'w' ) as out:
+      for cmd in cmds:
+        log = execute( cmd )
+        out.write( log )
